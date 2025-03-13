21001ff96c3604e0154c72de9a61e30942148460
xKratos19 <123822288+xKratos19@users.noreply.github.com>
2025-03-13 20:26:50 +0200

    Structure fix and gitignore with env and node_modules

--- Lab1/generate-keypair.ts
+++
@@ -1,7 +0,0 @@
-import { Keypair } from "@solana/web3.js";
-
-const keypair = Keypair.generate();
-
-console.log("Done - Keypair generated succesfully");
-console.log("Public key", keypair.publicKey.toBase58());
-console.log("Public key", keypair.secretKey);
